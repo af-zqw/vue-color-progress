67de1d3f4d008af787d36a76408c9dbe88286745
曾庆炜 <1274085986.com>
2019-12-16 14:31:07 +0800

    all

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Main :progress="50" label="msg" rate="slow"></Main>
+  </div>
+</template>
+
+<script>
+import Main from './components/Main'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    Main
+  }
+}
+</script>
+
+<style>
+</style>
